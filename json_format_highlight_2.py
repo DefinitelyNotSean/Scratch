6531d6f92c8c6658e691836dcbea308f3d82fac3
Sean O'Neill <109048729+DefinitelyNotSean@users.noreply.github.com>
2023-04-28 10:28:28 -0400

    Update json_format_highlight_2.py

--- json_format_highlight_2.py
+++ json_format_highlight_2.py
@@ -1,42 +1,25 @@
 import json
-from termcolor import colored
+from pygments import highlight
+from pygments.lexers import JsonLexer
+from pygments.formatters import TerminalFormatter, Terminal256Formatter
 
-def colorize_json_element(key, value):
-    key_color = 'cyan'
+def main():
+    json_string = input("Please enter JSON: ")
+    #json_string = '{"name": "John", "age": 30, "city": "New York", "isTrue": true, "isArray": ["hello", "world"] }'
+    
+    formatted_json = format_json(json_string)
+    highlighted_json = highlight_json(formatted_json)
+    print("Formatted and highlighted JSON:")
+    print(highlighted_json)
 
-    if isinstance(value, str):
-        value_color = 'green'
-    elif isinstance(value, (int, float)):
-        value_color = 'yellow'
-    elif isinstance(value, bool):
-        value_color = 'magenta'
-    elif value is None:
-        value_color = 'white'
-    else:
-        return colored(key, key_color), value
-
-    return colored(key, key_color), colored(value, value_color)
-
-def format_and_colorize_json(json_data, indent=0):
-    if isinstance(json_data, dict):
-        formatted_json = '{\n'
-        for key, value in json_data.items():
-            colored_key, colored_value = colorize_json_element(key, value)
-            formatted_json += '  ' * (indent + 1) + f'{colored_key}: {format_and_colorize_json(colored_value, indent + 1)},\n'
-        formatted_json = formatted_json.rstrip(',\n') + '\n' + '  ' * indent + '}'
-    elif isinstance(json_data, list):
-        formatted_json = '[\n'
-        for value in json_data:
-            formatted_json += '  ' * (indent + 1) + f'{format_and_colorize_json(value, indent + 1)},\n'
-        formatted_json = formatted_json.rstrip(',\n') + '\n' + '  ' * indent + ']'
-    else:
-        _, colored_value = colorize_json_element('', json_data)
-        formatted_json = colored_value
-
+def format_json(json_string):
+    json_data = json.loads(json_string)
+    formatted_json = json.dumps(json_data, indent=4, sort_keys=True)
     return formatted_json
 
-if __name__ == '__main__':
-    json_string = '{"name": "John", "age": 30, "city": "New York", "active": true, "pets": ["dog", "cat"], "extra": null}'
-    json_data = json.loads(json_string)
-    colored_formatted_json = format_and_colorize_json(json_data)
-    print(colored_formatted_json)
+def highlight_json(formatted_json):
+    highlighted_json = highlight(formatted_json, JsonLexer(), Terminal256Formatter())
+    return highlighted_json
+
+if __name__ == "__main__":
+    main()
