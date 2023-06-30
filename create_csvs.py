e8e4bfa9ff7cc88c7b7e0c03ba210af996817dab
Sean O'Neill <109048729+DefinitelyNotSean@users.noreply.github.com>
2023-06-30 15:34:47 -0400

    Update create_csvs.py

--- create_csvs.py
+++ create_csvs.py
@@ -11,14 +11,17 @@ def create_csv_from_excel(file_name, sheet_name):
 
     # create raw.csv
     raw_df = data.iloc[:, 0:4]
+    raw_df.dropna(how='all', inplace=True)
     raw_df.to_csv('output/raw.csv', index=False)
 
     # create refined.csv
     refined_df = data.iloc[:, 12:16]
+    refined_df.dropna(how='all', inplace=True)
     refined_df.to_csv('output/refined.csv', index=False)
 
     # create modeled.csv
     modeled_df = data.iloc[:, 23:27]
+    modeled_df.dropna(how='all', inplace=True)
     modeled_df.to_csv('output/modeled.csv', index=False)
 
 # call the function with file name and sheet name
