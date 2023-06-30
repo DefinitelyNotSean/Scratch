2eb68a33f999b4663ffc8a23bc2ea9e7f540f8b9
Sean O'Neill <109048729+DefinitelyNotSean@users.noreply.github.com>
2023-06-30 15:27:00 -0400

    Update create_csvs.py

--- create_csvs.py
+++ create_csvs.py
@@ -2,23 +2,23 @@ import pandas as pd
 import os
 
 def create_csv_from_excel(file_name, sheet_name):
-    # read the sheet of the excel file
-    df = pd.read_excel(file_name, sheet_name=sheet_name, engine='openpyxl')
+    # read the excel file, skipping the first 4 rows
+    data = pd.read_excel(file_name, sheet_name=sheet_name, skiprows=range(4), engine='openpyxl')
 
     # create output directory if it does not exist
     if not os.path.exists('output'):
         os.makedirs('output')
 
     # create raw.csv
-    raw_df = df.iloc[4:, 0:4]
+    raw_df = data.iloc[:, 0:4]
     raw_df.to_csv('output/raw.csv', index=False)
 
     # create refined.csv
-    refined_df = df.iloc[4:, 12:16]
+    refined_df = data.iloc[:, 12:16]
     refined_df.to_csv('output/refined.csv', index=False)
 
     # create modeled.csv
-    modeled_df = df.iloc[4:, 23:27]
+    modeled_df = data.iloc[:, 23:27]
     modeled_df.to_csv('output/modeled.csv', index=False)
 
 # call the function with file name and sheet name
