f5855f586116a89ec13c39fd25685c17730aba13
Sean O'Neill <109048729+DefinitelyNotSean@users.noreply.github.com>
2023-11-16 08:26:42 -0500

    Update can_delete.py

--- can_delete.py
+++ can_delete.py
@@ -6,3 +6,14 @@
         "overviewRulerColor": "rgba(255,215,0,0.7)"
     }
 ]
+
+    "todohighlight.keywords": [
+    {
+        "text": "/LOGGER\\.(info|debug)/",
+        "color": "#FFA07A", // Choose your preferred color
+        "backgroundColor": "transparent",
+        "overviewRulerColor": "rgba(255,160,122,0.7)",
+        "isRegexp": true
+    }
+]
+
